9c50dd28a7aa04ab9572c6a4fa6f9ebdcef0e569
@JBessa <jonathan.bessa@qivalio.com>
2022-09-30 23:28:50 +0200

    Ajout de la page galerie

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomepageController;
+use App\Http\Controllers\PhotoBoxController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [HomepageController::class, 'index']);
+Route::get('/galerie', [PhotoBoxController::class, 'index']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
